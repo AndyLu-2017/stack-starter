647e01d8d7d45f3cbc2955d993c5db01de27452d
AndyLu-2017 <31222383+AndyLu-2017@users.noreply.github.com>
2017-12-21 14:29:23 -1000

    Add files via upload

--- Stack.java
+++ Stack.java
@@ -14,13 +14,42 @@
 public class Stack implements StackInterface {
 
 	// Set up the basic elements of your data structure here.
-	
+	private String[] stack;
+	private int pointer;
 	/*
 	 * This is the constructor for the Stack class.  Notice
 	 * there is one integer parameter (the size of the stack).
 	 */
 	public Stack(int size) {
-		// Your code here
+		stack = new String[size];
+		pointer = 0;
+	}
+
+	@Override
+	public boolean push(String record) {
+		boolean added = false;
+		for(int i = 0; i < stack.length; i++) {
+			if(stack[i] == null && !added) {
+				stack[i] = record;
+				pointer = i;
+				added = true;
+			}
+		}
+		return added;
+	}
+
+	@Override
+	public String pop() {
+		String temp = stack[pointer];
+		stack[pointer] = null;
+		pointer -= 1;
+		return temp;
+	}
+
+	@Override
+	public void printStack() {
+		for(int i = pointer; i >= 0; i--) 
+			System.out.println(stack[i]);
 	}
 	
 	/*
